d6bf4651ccc0a33e858a384844704d0abd063b2f
prasadmahakal11 <141907873+prasadmahakal11@users.noreply.github.com>
2023-11-29 00:13:38 +0530

    Add files via upload

---
+++ BootMS-Proj05-EmpService/src/main/java/com/nt/rest/EmployeeOperationsController.java
@@ -0,0 +1,28 @@
+//EmployeeOperationsController.java
+package com.nt.rest;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/emp")
+@RefreshScope
+public class EmployeeOperationsController {
+	
+	@Value("${dbuser}")
+	private  String user;
+	
+	@Value("${dbpwd}")
+	private  String pwd;
+	
+	@GetMapping("/show")
+	public  ResponseEntity<String>  showData(){
+		return new ResponseEntity<String>(" db Properties from GITLab Account are ::"+user+"...."+pwd,HttpStatus.OK);
+	}
+
+}
---
+++ BootMS-Proj05-StudentService/src/main/java/com/nt/rest/StudentOperationsController.java
@@ -0,0 +1,28 @@
+//StudentOperationsController.java
+package com.nt.rest;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/stud")
+@RefreshScope
+public class StudentOperationsController {
+	
+	@Value("${dbuser}")
+	private  String user;
+	
+	@Value("${dbpwd}")
+	private  String pwd;
+	
+	@GetMapping("/show")
+	public  ResponseEntity<String>  showData(){
+		return new ResponseEntity<String>(" db Properties from GITLab Account are ::"+user+"...."+pwd,HttpStatus.OK);
+	}
+
+}
